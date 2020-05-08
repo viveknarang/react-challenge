e012566d59af7df98c79d1760c49f67fd903ccd3
Vivek Narang <vivek.narang10@gmail.com>
2020-05-08 18:05:46 -0400

    first commit ...

---
+++ README.md
@@ -0,0 +1,31 @@
+Your mission, if you choose to accept it, is to build out a list of all the Space X Launches
+by leveraging the SpaceX API:
+
+# API Endpoint https://api.spacexdata.com/v3/launches
+
+- You will dynamically call the API and populate _TWO_ lists
+
+- One list will render all _SUCCESSFUL_ launches
+- Another list will render all _FAILED_ launches
+
+  - Also provide the reason for the launch failure
+
+- Required fields for the list are as follows
+
+  - Flight number
+  - Mission Name
+  - Launch 
+  
+- Required fields for the list are as follows
+
+  - Flight number
+  - Mission Name
+  - Launch Year
+  - Launch Date
+  - Launch Site (by name)
+  - Mission Patch Image
+  - Mission Details
+
+- You'll be evaluated on a combination of skill, approach to problem solving and creativity
+- EXTRA CREDIT: Create a function that will sort the lists by either name or launch date/time
+
---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+// Replace the below url with: "https://api.spacexdata.com/v3/launches"
+const url = "http://localhost:5000/launches"
+
+function sortByMissionName(sortOnWho) {
+    sortOnWho.sort((a, b) => a.mission_name.localeCompare(b.mission_name));
+}
+
+function renderView(data, heading) {
+
+    return(
+      <div>
+          <hr/>
+          <h1>{heading}</h1>
+          <br/>
+          <br/>
+          <table>
+                <thead>
+                  <tr>
+                    <th>Flight number</th>
+                    <th>Mission Name</th>
+                    <th>Launch Year</th>
+                    <th>Launch Date</th>
+                    <th>Launch Site</th>
+                    <th>Mission Patch Image</th>
+                    <th>Mission Details</th>
+                  </tr>
+                </thead>
+          
+          {data.map((item) => {
+            
+              return (<tr key={item.flight_number}>
+                    <td>{item.flight_number}</td>
+                    <td width="200px">{item.mission_name}</td>
+                    <td width="200px">{item.launch_year}</td>
+                    <td width="200px">{item.launch_date_local}</td>
+                    <td width="200px">{item.launch_site.site_name}</td>
+                    <td width="200px"><img src={item.links.mission_patch} alt="Mission Patch" width="100px" height="100px"/></td>
+                    <td width="300px">{item.details}</td>
+              </tr>)
+
+          })}
+          </table>
+      </div>
+    )
+
+}
+
+class App extends React.Component {
+
+  state = {
+    loading: true,
+    successfulMissions: [],
+    failedMissions: []
+  }
+
+  componentDidMount() {
+
+      this.setState({loading: true});
+
+      axios.get(url).then((response) => {
+
+            this.setState({successfulMissions : response.data.filter((item) => item.launch_success)})
+            this.setState({failedMissions : response.data.filter((item) => !item.launch_success)})
+
+            sortByMissionName(this.state.successfulMissions);
+            sortByMissionName(this.state.failedMissions);
+
+            this.setState({loading: false});
+
+      });      
+
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+            { !this.state.loading ? (
+              <div>
+                    {renderView(this.state.successfulMissions, "Successful Missions")}
+                    {renderView(this.state.failedMissions, "Failed Missions")}
+              </div>
+            ):(
+                <span>Loading data please wait ...</span>  
+            )}
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default App;
