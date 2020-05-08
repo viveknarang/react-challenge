9527802f4d13591f31aa5a9da7c97d8d24e42ce9
Vivek Narang <vivek.narang10@gmail.com>
2020-05-08 19:08:34 -0400

    adding uuid for uniqe keys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+const { v4: uuidv4 } = require('uuid');
 
 // Replace the below url with: "https://api.spacexdata.com/v3/launches"
 const url = "http://localhost:5000/launches"
@@ -29,10 +30,10 @@ function renderView(data, heading) {
                     <th>Mission Details</th>
                   </tr>
                 </thead>
-          
+                <tbody>
           {data.map((item) => {
             
-              return (<tr key={item.flight_number}>
+              return (<tr key={uuidv4()}>
                     <td>{item.flight_number}</td>
                     <td width="200px">{item.mission_name}</td>
                     <td width="200px">{item.launch_year}</td>
@@ -43,6 +44,7 @@ function renderView(data, heading) {
               </tr>)
 
           })}
+               </tbody>
           </table>
       </div>
     )
@@ -54,7 +56,8 @@ class App extends React.Component {
   state = {
     loading: true,
     successfulMissions: [],
-    failedMissions: []
+    failedMissions: [],
+    issueWithEndPoint: false,
   }
 
   componentDidMount() {
@@ -65,12 +68,13 @@ class App extends React.Component {
 
             this.setState({successfulMissions : response.data.filter((item) => item.launch_success)})
             this.setState({failedMissions : response.data.filter((item) => !item.launch_success)})
-
+            console.log(this.state.failedMissions);
             sortByMissionName(this.state.successfulMissions);
             sortByMissionName(this.state.failedMissions);
-
             this.setState({loading: false});
 
+      }).catch((message) => {
+            this.setState({issueWithEndPoint: true});
       });      
 
   }
